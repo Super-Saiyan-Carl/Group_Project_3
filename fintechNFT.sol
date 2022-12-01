c7a5f99d3bb5d486c36398588dfd9891109c2996
Super Saiyan Carl <101432134+Super-Saiyan-Carl@users.noreply.github.com>
2022-11-30 23:39:53 -0700

    additions
    added a few imports and uri/safemint functions

--- fintechNFT.sol
+++ fintechNFT.sol
@@ -2,17 +2,29 @@
 pragma solidity ^0.8.7;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import "@openzeppelin/contracts@4.8.0/token/ERC721/extensions/ERC721Enumerable.sol";
 import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts@4.8.0/utils/Counters.sol";
 
-contract SimpleMintContract is ERC721, Ownable {
+contract fintechNFT is ERC721, Ownable {
     uint256 public mintPrice = 0.01 ether;
     uint256 public totalSupply;
     uint256 public maxSupply;
     bool public isMintEnabled;
     mapping(address => uint256) public mintedWallets;
 
-    constructor() payable ERC721('Simple Mint', 'SIMPLEMINT') {
+    constructor() payable ERC721('Fintech NFT', 'NFT') {
         maxSupply = 1000;
+    }
+    
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://api.fintechnft.com/tokens/";
+    }
+    
+    function safeMint(address to) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
     }
 
     function toggleIsMintEnabled() external onlyOwner {
