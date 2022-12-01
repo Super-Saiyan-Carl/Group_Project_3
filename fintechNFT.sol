32e04c65a6c90a1fae41eddfa0441fb0f4d52e6f
Super Saiyan Carl <101432134+Super-Saiyan-Carl@users.noreply.github.com>
2022-12-01 00:03:51 -0700

    updated everything

--- fintechNFT.sol
+++ fintechNFT.sol
@@ -1,49 +1,52 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import "@openzeppelin/contracts@4.8.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.8.0/token/ERC721/extensions/ERC721Enumerable.sol";
-import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts@4.8.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.8.0/utils/Counters.sol";
 
-contract fintechNFT is ERC721, Ownable {
-    uint256 public mintPrice = 0.01 ether;
-    uint256 public totalSupply;
-    uint256 public maxSupply;
-    bool public isMintEnabled;
-    mapping(address => uint256) public mintedWallets;
+contract fintechNFT is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
 
-    constructor() payable ERC721('Fintech NFT', 'NFT') {
-        maxSupply = 1000;
-    }
-    
+    Counters.Counter private _tokenIdCounter;
+    uint256 public mintRate = 0.01 ether;
+    uint public maxSupply_ = 1000;
+
+    constructor() ERC721("FintechNFT", "NFT") {}
+
     function _baseURI() internal pure override returns (string memory) {
         return "https://api.fintechnft.com/tokens/";
     }
-    
-    function safeMint(address to) public {
-        uint256 tokenId = _tokenIdCounter.current();
+
+    function safeMint(address to) public payable {
+        require(totalSupply() < maxSupply_, "Max amount minted for wallet.");
+        require(msg.value >= mintRate, "Not enough ether to mint.");
         _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+        _safeMint(to, _tokenIdCounter.current());
     }
 
-    function toggleIsMintEnabled() external onlyOwner {
-        isMintEnabled = !isMintEnabled;
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
-    function setMaxSupply(uint256 maxSupply_) external onlyOwner {
-        maxSupply = maxSupply_;
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
-    
-    function mint() external payable {
-        require(isMintEnabled, 'minting not enabled');
-        require(mintedWallets[msg.sender] < 3, 'exceeds max per wallet');
-        require(msg.value == mintPrice, 'wrong value');
-        require(maxSupply > totalSupply, 'sold out');
-    
-        mintedWallets[msg.sender]++;
-        totalSupply++;
-        uint256 tokenId = totalSupply;
-        _safeMint(msg.sender, tokenId);
+
+    function withdraw() public onlyOwner {
+        require(address(this).balance > 0, "Balance is 0");
+        payable(owner()).transfer(address(this).balance);
     }
 }
+
